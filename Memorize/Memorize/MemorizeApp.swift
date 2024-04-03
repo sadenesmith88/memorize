b843399ad9044bab46356e53627d1d8ddb569b1c
Sade Nesmith <sadework21@gmail.com>
2024-04-03 14:33:41 -0400

    initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by sade on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
