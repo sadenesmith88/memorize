aa91ae8cec882a773d48dd45ea563118d8e92701
Sade Nesmith <sadework21@gmail.com>
2024-04-03 15:09:39 -0400

    toggleable isFaceUp

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -22,19 +22,24 @@ struct ContentView: View {
 
 
 struct CardView: View {
-    var isFaceUp: Bool = true
+   @State var isFaceUp: Bool = true
     
     var body: some View{
         ZStack {
+            //local variable to store rounded rectangle
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 5)
                 Text("😀").font(.largeTitle)
                 
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
         }
+        .onTapGesture(perform:  {
+            isFaceUp.toggle()
+        })
     }
 }
 
